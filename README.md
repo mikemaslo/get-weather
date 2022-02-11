f91c8fe83be5c290f57808904412b9ef3e329acd
mikhail.maslennikov <m.maslennikov@skyeng.ru>
2022-02-11 14:23:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Get weather
+
+###[Link to live website]()
+
+## Hello! It's my first pet project.
+
+### Stack technologies:
+ - JavaScript
+ - CSS
+ - HTML
+
+### What I learn after complete this project:
+ 1. In first time used Flexbox in CSS
+ 2. Learned use CSS animation
+ 3. Solidified the knowledge of HTML
+ 4. How to work promise in JS
+ 5. How to get access for DOM from JS
+ 6. Solidified the knowledge of ES6 syntax
+ 7. How to get data from open API
+ 8. Tried using the BEM methodology
+
+### Please contact me if you have any suggestions or have any problems using the website
+ - Telegram: [@mikemaslo](http://t.me/mikemaslo)
+ - Email: [mike.maslo96@gmail.com](mailto:mike.maslo96@gmail.com)
---
+++ script.js
@@ -0,0 +1,147 @@
+const API_KEY = 'a8178da96667451ab1e140038220902'
+const API_URL = 'https://api.weatherapi.com/v1'
+
+const WEATHER_CURRENT = '/current.json'
+const WEATHER_FORECAST = '/forecast.json'
+const WEATHER_SEARCH = '/search.json'
+const WEATHER_HISTORY = '/history.json'
+const WEATHER_TIMEZONE = '/timezone.json'
+const WEATHER_SPORTS = '/sports.json'
+const WEATHER_ASTRONOMY = '/astronomy.json'
+const WEATHER_IPLOOKUP = '/ip.json'
+
+let lang = '&lang=' + document.documentElement.lang
+
+let weatherLoader = document.getElementById('loader')
+let weatherMain = document.getElementById('weather-main')
+let weatherIcon = document.getElementById('weather-icon')
+let weatherTemp = document.getElementById('weather-temp')
+let weatherAdditionalBlock = document.getElementById('weather-additional-block')
+let weatherTempBlock = document.getElementById('weather-temp-block')
+let weatherCountry = document.getElementById('weather-country')
+let weatherTime = document.getElementById('weather-time')
+let weatherCondition = document.getElementById('weather-condition')
+let weatherFeels = document.getElementById('weather-feels')
+let weatherHumidity = document.getElementById('weather-humidity')
+let weatherPrecip = document.getElementById('weather-precip')
+let weatherWind = document.getElementById('weather-wind')
+let weatherCloud = document.getElementById('weather-cloud')
+let weatherPressure = document.getElementById('weather-pressure')
+let weatherVisibility = document.getElementById('weather-visibility')
+let weatherUvIndex = document.getElementById('weather-uv')
+
+let dataObject = {}
+
+const weekDays = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+
+const convertKphMph = function (kph) {
+    return Math.round(kph * 1000 / 3600) + ' m/s'
+}
+
+const getIcon = function(iconUrl) {
+    let iconCode = iconUrl.match(/64x64\/[a-z]+\/\d+\.png/gm)[0]
+        return `no-repeat center/100% url("./img/${iconCode}")`
+}
+
+const convertUnix = function (unixTimestamp) {
+    let unixDate = new Date(unixTimestamp*1000)
+    let weekday = weekDays[unixDate.getDay()]
+    let hours = unixDate.getHours()
+    let minutes = unixDate.getMinutes()
+    if (hours < 10) {
+        hours = '0' + hours
+    } else {hours}
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    } else {minutes}
+    return weekday + ' ' + hours + ':' + minutes
+}
+
+function getWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successful, error);
+    }
+
+    //If error, alert user ...unable to obtain temperature
+    function error(error) {
+        window.alert("Unable to retrieve weather! This might be due to having your location setting turned off on your mobile device " +
+            "or from denying location access on desktop. Please note that if you are using FireFox, that there is currently a bug with " +
+            "geolocation. Please use a different browser! \n\n" + " {Error message: " + error.message + "}");
+        weatherIcon.innerHTML = '<h2>Unable to retrieve weather!</h2>';
+    }
+
+    function successful(geoLocation) {
+        let lat = geoLocation.coords.latitude
+        let lon = geoLocation.coords.longitude
+        let query = '&q=' + lat + ',' + lon + lang
+        let url = API_URL + WEATHER_CURRENT + '?key=' + API_KEY + query
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                dataObject = data
+                let locationName = data.location.region + ', ' + data.location.country
+                    weatherTemp.innerHTML = Math.round(data.current.temp_c)
+                    weatherIcon.style.background = getIcon(data.current.condition.icon)
+                    weatherCountry.innerHTML = `<h4>${locationName}</h4>`
+                    weatherTime.innerHTML = convertUnix(data.location.localtime_epoch)
+                    weatherCondition.innerHTML = data.current.condition.text
+                    weatherFeels.innerHTML = Math.round(data.current.feelslike_c) + '°C'
+                    weatherHumidity.innerHTML = data.current.humidity + ' %'
+                    weatherPrecip.innerHTML = data.current.precip_mm + ' mm'
+                    weatherWind.innerHTML = data.current.wind_dir + ' ' + convertKphMph(data.current.wind_kph)
+                    weatherCloud.innerHTML = data.current.cloud + ' %'
+                    weatherPressure.innerHTML = Math.round(data.current.pressure_in) + ' in'
+                    weatherVisibility.innerHTML = data.current.vis_km + ' km'
+                    weatherUvIndex.innerHTML = data.current.uv
+            })
+            .then(() => {
+                weatherAdditionalBlock.style.opacity = '100'
+                weatherTempBlock.style.opacity = '100'
+                weatherLoader.style.display = 'none'
+            })
+    }
+}
+
+const switcher = function() {
+    let fahrenheit = document.getElementById('fahrenheit');
+    let celsius = document.getElementById('celsius');
+    if (celsius.checked) {
+        weatherFeels.innerHTML = Math.round(dataObject.current.feelslike_c) + '°C'
+        weatherTemp.innerHTML = Math.round(dataObject.current.temp_c)
+    } else if (fahrenheit.checked) {
+        weatherFeels.innerHTML = Math.round(dataObject.current.feelslike_f) + '°F'
+        weatherTemp.innerHTML = Math.round(dataObject.current.temp_f)
+    }
+}
+
+const bookmark = function() {
+    let bookmarkElement = document.getElementById('bookmark')
+    let aboutElement = document.getElementById('about')
+    // start animation for bookmark
+    bookmarkElement.style.animationName = 'bookmark-increase, bookmark-increase-text,' +
+                                          'bookmark-decrease, bookmark-decrease-text,' +
+                                          'hide-me'
+    bookmarkElement.style.animationDuration = '1s, 1s, 3s, 2s, 2s'
+    bookmarkElement.style.animationDelay = '0s, 0s, 1s, 1s, 1s'
+    bookmarkElement.style.animationTimingFunction = 'linear'
+    bookmarkElement.style.animationFillMode = 'forwards'
+    bookmarkElement.style.cursor = 'default'
+    // start animation for about block
+    aboutElement.style.animationName = 'about-drop-down, show-me'
+    aboutElement.style.animationDuration = '3s'
+    aboutElement.style.animationDelay = '1.5s'
+    aboutElement.style.animationTimingFunction = 'cubic-bezier(.49,.46,.51,1.13)'
+    aboutElement.style.animationFillMode = 'forwards'
+    // start animation for main weather block
+    weatherMain.style.animationName = 'hide-bottom-border-radius'
+    weatherMain.style.animationDuration = '3s'
+    weatherMain.style.animationDelay = '1.5s'
+    weatherMain.style.animationTimingFunction = 'linear'
+    weatherMain.style.animationFillMode = 'forwards'
+}
+
+
+getWeather()
+
+
